b22c98655cf37c6d499c537c7b31702e14237adf
25kq1a5462-ship-it <25kq1a5462@pace.ac.in>
2025-11-26 22:46:42 +0530

    Update Loop.c

--- Loop.c
+++ Loop.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
+intmain()
 {
- int i;
-clrscr();
-scanf("%d",&i);
-printf("Enter the values");
-for(i=10;i>=10;i++)
+ int i,a[5],n;
+printf("Enter the no of elements");
+scanf("%d",n);
+printf("Enter the values:");
+for(i=0;i<n;i++)
 {
-printf("%d",i);
+ scanf("%d",a[i]);
+}
+printf("Entered value");
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+return 0;
 }
